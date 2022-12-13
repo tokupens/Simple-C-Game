b522c4ebc80e3c0ab1d39d48690333a395dcf24e
Tokupensx07 <119869342+tokupens@users.noreply.github.com>
2022-12-13 18:18:11 +0700

    Create car.f

---
+++ car.f
@@ -0,0 +1,18 @@
+//---------CAR.f---------------
+#ifndef CAR
+#define CAR
+
+#include "object.h"
+
+class Car: public object
+{
+private:
+ int texture;
+public:
+ Car(double px,double py);
+ void draw();
+ void update();
+ void control(double cx);
+ void loadTexture();
+};
+#endif
